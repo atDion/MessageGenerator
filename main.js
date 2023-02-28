576a281d9ec708ceee848534139aff11db3bfe99
atdion <85219817+atdion@users.noreply.github.com>
2023-02-28 21:05:40 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/* Message Generator 
+Moet kunnen:
+1. console.log(randomMessage()) output message bestaande uit 3 stukken. bijv; I feel a lot of passion in your work.
+
+*/
+
+
+const generateMessage = () => {
+    const emotions = ["anger", "rage", "interest", "serenity", "acceptance", "trust", "admiration", "passion", "surprise", "grief", "sadness"]
+    const level = ["a lot of", "some", "enormous amounts of", "a hint of"]
+    const subject = ["work", "hobby", "secret life", "relationship", "marriage", "dog"]
+    let randomNumber
+}
