b9aa4fa0ca064dc7d2bca4bb7864720d213f6768
atdion <85219817+atdion@users.noreply.github.com>
2023-02-28 21:19:33 +0100

    Added all functionalities

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 /* Message Generator 
 Moet kunnen:
 1. console.log(randomMessage()) output message bestaande uit 3 stukken. bijv; I feel a lot of passion in your work.
-
 */
 
+const randomNumber = (num) => {
+    return Math.floor(Math.random() * num)
+}
+
+const collectiveKnowledge = {
+    emotion: ["anger", "rage", "interest", "serenity", "acceptance", "trust", "admiration", "passion", "surprise", "grief", "sadness", "disgust"],
+    level: ["a lot of", "some", "enormous amounts of", "a hint of"],
+    subject: ["work", "hobby", "secret life", "relationship", "marriage", "dog"]
+}
 
 const generateMessage = () => {
-    const emotions = ["anger", "rage", "interest", "serenity", "acceptance", "trust", "admiration", "passion", "surprise", "grief", "sadness"]
-    const level = ["a lot of", "some", "enormous amounts of", "a hint of"]
-    const subject = ["work", "hobby", "secret life", "relationship", "marriage", "dog"]
-    let randomNumber
+
+    const emotion = collectiveKnowledge.emotion[randomNumber(collectiveKnowledge.emotion.length - 1)]
+    const level = collectiveKnowledge.level[randomNumber(collectiveKnowledge.level.length - 1)]
+    const subject = collectiveKnowledge.subject[randomNumber(collectiveKnowledge.subject.length - 1)]
+
+    return `I sense ${emotion} in your ${subject}, ${level}.`;
 }
+
+console.log(generateMessage());
